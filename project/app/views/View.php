baa600045faff3a743464d56ee32fea44100606a
Egor-Guzaev <enotguzaev604@gmail.com>
2025-03-26 11:51:02 +0300

    rework all arh

--- project/app/views/View.php
+++ project/app/views/View.php
@@ -1,12 +1,10 @@
 <?php
 class View {
     private $vars = [];
-    private $templateDir = 'app/views/';
+    private $templateDir;
 
     public function __construct($templateDir = null) {
-        if ($templateDir) {
-            $this->templateDir = $templateDir;
-        }
+        $this->templateDir = $templateDir ?: ROOT_DIR . '/app/views/';
     }
 
     public function __set($name, $value) {
@@ -26,8 +24,20 @@ class View {
         // Начинаем буферизацию вывода
         ob_start();
 
+        // Проверяем существование шаблона
+        $layoutFile = $this->templateDir . '/templates/layout.php';
+        $templateFile = $this->templateDir . '/' . $template . '.php';
+
+        if (!file_exists($layoutFile)) {
+            die("Файл шаблона layout.php не найден по пути: {$layoutFile}");
+        }
+
+        if (!file_exists($templateFile)) {
+            die("Файл шаблона {$template}.php не найден по пути: {$templateFile}");
+        }
+
         // Подключаем шаблон
-        include($this->templateDir . 'templates/layout.php');
+        include($layoutFile);
 
         // Выводим буфер
         echo ob_get_clean();
